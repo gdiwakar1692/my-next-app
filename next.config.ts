ab75b7bccd952554f87ff41b95d67b915ba90512
-angular-localgit <-angular-localgit>
2025-02-19 15:06:11 +0000

    IC

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    PORT: process.env.PORT || '3000', // Default to 3000 if PORT is not set
+  },
 };
 
 export default nextConfig;
